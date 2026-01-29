340ce7bcc6fc094556e9aa136fc0c622a7488262
Your Name <tsuchiya.h.24.kdgn@gmail.com>
2026-01-29 16:42:35 +0900

    Remove add_game_and_type migration

--- database/migrations/2026_01_23_071054_add_game_and_type_to_articles_table.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Schema::table('articles', function (Blueprint $table) {
-    $table->string('game')->nullable()->after('id');        
-    $table->string('type')->default('update')->after('game'); 
-});
-
